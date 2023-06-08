57465cd5d6f6d5f5bb5508f6d314c1f9bee4a2ea
Alfredo Silva <a.alfredops@gmail.com>
2023-06-08 08:16:32 -0400

    TwoSum [ Finished ]

--- TwoSum/src/Main.java
+++ TwoSum/src/Main.java
@@ -11,32 +11,41 @@ public class Main {
         // OBS => Each input have exactly one solution
         // OBS => You may not use the same element twice.
 
-//        int[] test = {2,7,11,15};
-//        int[] secondTest = {3,2,4};
-//        int[] thirdTest = {3,3};
+        int[] test = {2,7,11,15};
+        int[] secondTest = {3,2,4};
+        int[] thirdTest = {3,3};
         int[] leetCode = {2,4,11,3};
+        int[] leetCode1 = {2,5,5,11};
 
-//        System.out.println(Arrays.toString(twoSum(test,9)));
-//        System.out.println(Arrays.toString(twoSum(secondTest,6)));
-//        System.out.println(Arrays.toString(twoSum(thirdTest,6)));
+        System.out.println(Arrays.toString(twoSum(test,9)));
+        System.out.println(Arrays.toString(twoSum(secondTest,6)));
+        System.out.println(Arrays.toString(twoSum(thirdTest,6)));
         System.out.println(Arrays.toString(twoSum(leetCode,6)));
+        System.out.println(Arrays.toString(twoSum(leetCode1,10)));
     }
 
     public static int[] twoSum(int[] nums, int target)
     {
         int length = nums.length;
         int[] result = {0,0};
+        boolean found = false;
 
         for(var i = 0; i < length; i++)
         {
-            for(var j = 0; j < length; j++)
+            for(var j = 1; j < length; j++)
             {
-                if(nums[j] + nums[i] == target)
+                if(nums[j] + nums[i] == target && j != i)
                 {
-                    result[0] = j;
-                    result[1] = i;
+                    result[0] = i;
+                    result[1] = j;
+                    found = true;
                     break;
                 }
+            }
+
+            if(found)
+            {
+                break;
             }
         }
         return result;
